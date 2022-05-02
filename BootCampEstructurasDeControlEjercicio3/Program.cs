8d91c4906c37dfac81ac2d36e7b23d5195c7aa9c
Lautaro De Simone <sistemas.desimone@gmail.com>
2022-05-02 14:09:42 -0300

    Add project files.

---
+++ BootCampEstructurasDeControlEjercicio3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BootCampEstructurasDeControlEjercicio3
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            List<int> numbersToAdd = new List<int>();
+            int summatoryOfTheList = 0;
+            int i = 0;
+            int median;
+            
+            while(summatoryOfTheList < 150)
+            { 
+                Console.WriteLine("Let's add numbers up to 150");
+            
+                numbersToAdd.Add(Convert.ToInt32(Console.ReadLine()));
+                i++;
+            
+            }
+
+            foreach(int number in numbersToAdd)
+            {
+                summatoryOfTheList += number;
+
+            }
+
+            median = summatoryOfTheList / i;
+
+            
+        }
+    }
+}
